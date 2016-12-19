8fd99fe246982cee5a844763294fe3c0af17fae1
Thomas Pan <tpan@e1z2r5p10.42.us.org>
2016-12-19 15:52:34 -0800

    fixing object files and wordlength

--- ft_wordlength.c
+++ ft_wordlength.c
@@ -6,19 +6,21 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 14:02:45 by tpan              #+#    #+#             */
-/*   Updated: 2016/12/16 14:13:25 by tpan             ###   ########.fr       */
+/*   Updated: 2016/12/19 12:51:32 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_wordlength(char *str,int i, char delimiter)
+int	ft_wordlength(char *str, int i, char delimiter)
 {
 	size_t len;
 
 	len = 0;
 	while (str[i] != '\0' && str[i] != delimiter)
+	{
 		i++;
 		len++;
+	}
 	return (i);
 }
